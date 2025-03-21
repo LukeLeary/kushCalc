ec82c2228883451717a0dc0d94e8cbe03d8c95ab
Pondering Ape <59585655+LukeLeary@users.noreply.github.com>
2025-03-21 15:40:39 -0400

    Update randPic.js

--- randPic.js
+++ randPic.js
@@ -1,20 +1,20 @@
 const pics = [
-'media/turkeyJ.png',
-'media/gingerJ.png',
-'media/braidedJ.png',
-'media/crossJ.jpg',
-'media/tulipJ.png',
-'media/planeJ.png',
-'media/x-wingJ.png',
-'media/cactusJ.png'
+    'media/crossJ.jpg',
+    'media/turkeyJ.png',
+    'media/gingerJ.png',
+    'media/braidedJ.png',
+    'media/tulipJ.png',
+    'media/planeJ.png',
+    'media/x-wingJ.png',
+    'media/cactusJ.png'
 ];
 
 
 const names = [
+    'Cross Joint',
     'Turkey Joint',
     'Ginger Gas',
     'Braided Bogie',
-    'Cross Joint',
     'tulip Joint',
     'Plane Joint',
     'X-wing Joint',
@@ -22,10 +22,10 @@ const names = [
 ];
 
 const links = [
+    'https://youtu.be/6x1KM579Pbs?feature=shared',
     'https://youtu.be/-jzy_--z-Yo?si=jm1W8_q-sKa9dRMd',
     'https://youtu.be/pYK4txwVgJ0?si=eJ6b4h5x2AkVPJxS',
     'https://youtu.be/LyKswcDG8Qo?feature=shared',
-    'https://youtu.be/6x1KM579Pbs?feature=shared',
     'https://youtu.be/dht2Lh46HYI?feature=shared',
     'https://youtu.be/Vt6qd6ly8To?feature=shared',
     'https://youtu.be/l6vU1pP1XNA?si=zlclGerUBSduldEc',
@@ -37,23 +37,44 @@ const link = document.getElementById('tutorialLink');
 const nameJ = document.getElementById('jointName');
 
 
-let lastIndex = 3;
-let a = 3;
+let shuffledIndices = [];
+let currentPosition = 0;
 
-pic.src = pics[a]
-nameJ.innerText = names[a]
-link.href = links[a];
+// Initialize with cross J
+pic.src = pics[0];
+nameJ.innerText = names[0];
+link.href = links[0];
+
+// Function to create a new shuffled sequence
+function createShuffledSequence() {
+    // Create array with all indices
+    shuffledIndices = Array.from(Array(pics.length).keys());
+    
+    // Shuffle the array using Fisher-Yates algorithm
+    for (let i = shuffledIndices.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledIndices[i], shuffledIndices[j]] = [shuffledIndices[j], shuffledIndices[i]];
+    }
+    
+    // Reset the position
+    currentPosition = 0;
+}
+
+// Create initial shuffled sequence
+createShuffledSequence();
 
 function showImage() {
-
-
-    do {
-        a = Math.floor(Math.random() * pics.length);
-    } while (a === lastIndex && pics.length > 1);
-
-    lastIndex = a;
-
-    nameJ.innerText = names[a];
-    pic.src = pics[a];
-    link.href = links[a];
+    // If we've gone through all images, create a new shuffled sequence
+    if (currentPosition >= shuffledIndices.length) {
+        createShuffledSequence();
+    }
+    
+    // Get the next index from our shuffled sequence
+    const index = shuffledIndices[currentPosition];
+    currentPosition++;
+    
+    // Update display
+    nameJ.innerText = names[index];
+    pic.src = pics[index];
+    link.href = links[index];
 }
